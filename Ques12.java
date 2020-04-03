370cc374cf4a5ebf455b4b2155af486ccd2893dc
ayushirohilla2020 <60682404+ayushirohilla2020@users.noreply.github.com>
2020-04-04 01:58:53 +0530

    Add files via upload

---
+++ Ques12.java
@@ -0,0 +1,24 @@
+package assignment;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Ques12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver","/home/shivam/Desktop/Chrome Driver/chromedriver_linux64/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://demo.automationtesting.in/Register.html");
+		driver.findElement(By.xpath("//input[@placeholder='First Name']")).click();
+		driver.findElement(By.xpath("//input[@placeholder='First Name']")).sendKeys("Ayushi");
+		driver.findElement(By.xpath("//input[@placeholder='Last Name']")).click();
+		driver.findElement(By.xpath("//input[@placeholder='Last Name']")).sendKeys("Rohila");
+		driver.findElement(By.xpath("//input[@type='email']")).click();
+		driver.findElement(By.xpath("//input[@type='email']")).sendKeys("ayushi.rohilla@tothenew.com");
+		
+
+	}
+
+}
