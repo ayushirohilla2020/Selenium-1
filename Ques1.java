6595e6b757966333e6d038d244d6beab57fd61e0
ayushirohilla2020 <60682404+ayushirohilla2020@users.noreply.github.com>
2020-04-04 00:18:36 +0530

    Add files via upload
    Write a script to open the Firefox browser and open https://www.google.com/ URL.

---
+++ Ques1.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Ques1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.gecko.driver","/home/ayushi/Desktop/geckodriver");
+		WebDriver driver = new FirefoxDriver();
+	    driver.get("https://www.google.com/");
+
+	}
+
+}
