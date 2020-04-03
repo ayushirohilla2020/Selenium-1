4f797bfa7e56ac19ae9e8eac93eabdb2af16f003
ayushirohilla2020 <60682404+ayushirohilla2020@users.noreply.github.com>
2020-04-04 00:21:56 +0530

    Add files via upload
    Write a script to launch the Google Chrome browser and open https://www.google.com/ URL.

---
+++ Ques2.java
@@ -0,0 +1,16 @@
+package assignment;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Ques2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver","/home/ayushi/Desktop/chromedriver");
+		WebDriver driver = new ChromeDriver();
+	    driver.get("https://www.google.com");
+
+	}
+
+}
