f9daffcfc8d171a40a1f438bad296bee99e396ed
ayushirohilla2020 <60682404+ayushirohilla2020@users.noreply.github.com>
2020-04-04 01:30:27 +0530

    Add files via upload
     Write a script to open http://www.tothenew.com/  and then navigate to the contact us page using 
    1) XPath 
    2) ID

---
+++ Ques4.java
@@ -0,0 +1,26 @@
+package assignment;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Ques4 {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "/home/ayushi/Documents/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+	    driver.get("https://www.tothenew.com");
+	    driver.findElement(By.xpath("(//a[@id='h-contact-us'])[2]")).click();
+	    //Thread.sleep(2000);
+	    //driver.navigate().back();
+	    //driver.findElement(By.id("h-contact-us")).click(); 
+	    //Note: this id is not unique and there are more than one element pointed by this id.. so this is not working.
+
+	}
+
+}
+
